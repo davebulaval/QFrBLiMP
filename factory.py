9bd8d4dc72ef2df1f2c1c96ae50c7cf5866b8469
davebulaval <david.beauchemin.5@ulaval.ca>
2024-05-20 16:46:28 -0400

    more robust llama loading

--- factory.py
+++ factory.py
@@ -12,7 +12,7 @@ def model_tokenizer_factory(model_name, device, token):
     if "gpt" in model_name:
         model = GPT2LMHeadModel.from_pretrained(model_name).to(device)
         tokenizer = GPT2TokenizerFast.from_pretrained(model_name)
-    elif "llama" in model_name:
+    elif "llama" in model_name.lower():
         model = AutoModelForCausalLM.from_pretrained(
             model_name,
             token=token,

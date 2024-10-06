3b086c75ca473951b22ca5183feb3b14e76d6cb2
davebulaval <david.beauchemin.5@ulaval.ca>
2024-10-06 08:24:10 -0400

    iaa and utf8

---
+++ handle_utf8_and_merge.py
@@ -0,0 +1,18 @@
+# We use load_dataset from HG datasets library since it handle well JSONL
+import os
+
+from datasets import load_dataset
+
+dir_path = os.path.join("datastore", "FrBLiMP", "annotated")
+
+files = os.listdir(dir_path)
+
+annotated_data = load_dataset(
+    dir_path,
+    data_files=files,
+)
+
+annotated_data["train"].to_json(
+    os.path.join(dir_path, "fr_blimp_annotated.jsonl"),
+    force_ascii=False,
+)

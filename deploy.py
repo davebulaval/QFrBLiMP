a5360470ff5a949015e8023152eed0866377770a
davebulaval <david.beauchemin.5@ulaval.ca>
2024-10-08 18:26:32 -0400

    minor code fix

--- deploy.py
+++ deploy.py
@@ -14,14 +14,16 @@ names = [
     "Marc",
 ]
 
+dataset_size = "1711"
+
 ports = [x for x in range(8080, 8080 + len(names))]
 
 for name, port in zip(names, ports):
-    db_name = f"frblimp_50_{name}"
+    db_name = f"frblimp_{dataset_size}_{name}"
 
     call = (
         f"PRODIGY_PORT={port} python -m prodigy fr_blimp {db_name} "
-        f"../datastore/FrBLiMP/fr_blimp_50_sentences.jsonl -F recipe.py"
+        f"../datastore/FrBLiMP/unannotated/fr_blimp_{dataset_size}_sentences.jsonl -F recipe.py"
     )
 
     url = f"http://15.157.58.162:{port}/?session={name}"

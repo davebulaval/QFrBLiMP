3e996c390e4024e9c42f094b29bfc00bccddaa6c
davebulaval <david.beauchemin@ift.ulaval.ca>
2025-07-13 12:58:33 -0400

    add results and predictioons

--- article_tables.py
+++ article_tables.py
@@ -4,15 +4,17 @@ from datasets import load_dataset
 
 data = load_dataset(
     "json",
-    data_dir=os.path.join("datastore", "QFrBLiMP"),
+    data_dir=os.path.join("datastore", "QFrBLiMP", "release"),
     data_files=["qfrblimp.jsonl"],
 )
 
 print("a")
 
+data["train"].to_csv("men_colise.csv", index=False)
+
 data_split = load_dataset(
     "json",
-    data_dir=os.path.join("datastore", "QFrBLiMP"),
+    data_dir=os.path.join("datastore", "QFrBLiMP", "release"),
     data_files={
         "train": "qfrblimp_train.jsonl",
         "dev": "qfrblimp_dev.jsonl",

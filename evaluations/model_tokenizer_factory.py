8e85d72ae97c6a449eec55b86d8455dc43759bb7
davebulaval <david.beauchemin@ift.ulaval.ca>
2025-09-12 22:47:25 -0400

    fix lucie

--- evaluations/model_tokenizer_factory.py
+++ evaluations/model_tokenizer_factory.py
@@ -41,6 +41,7 @@ def model_tokenizer_factory(model_name, device, token, seed: int = 42):
         or "alpaca" in model_name.lower()
         or "mixtral" in model_name.lower()
         or "claire" in model_name.lower()
+        or "lucie" in model_name.lower()
     ):
         bnb_configs = BitsAndBytesConfig(load_in_8bit=True, low_cpu_mem_usage=True)
         model = AutoModelForCausalLM.from_pretrained(
@@ -52,7 +53,7 @@ def model_tokenizer_factory(model_name, device, token, seed: int = 42):
         tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
         if "mixtral" in model_name.lower():
             tokenizer.pad_token = tokenizer.eos_token
-    elif "flan" in model_name.lower() or "lucie" in model_name.lower():
+    elif "flan" in model_name.lower():
         bnb_configs = BitsAndBytesConfig(load_in_8bit=True, low_cpu_mem_usage=True)
         model = AutoModelForSeq2SeqLM.from_pretrained(
             model_name,
